9b17fbec9c596e800c19162a69734a96d261a616
Misha Wagner <mishajw@gmail.com>
2018-01-27 18:05:58 +0000

    Add single_thread_bscan function

--- src/block_scan.cu
+++ src/block_scan.cu
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <math.h>
+
+#include "common.h"
 
 #define LENGTH 10
 #define BLOCK_SIZE 1024
@@ -18,14 +21,59 @@ void host_bscan(num_t *xs, num_t *ys, size_t length) {
   }
 }
 
+__global__ 
+void single_thread_bscan(num_t *xs, num_t *ys, size_t length) {
+  int global_index = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+
+  if (global_index > 0) {
+    return;
+  }
+
+  ys[0] = xs[0];
+  for (size_t i = 1; i < length; i++) {
+    ys[i] = ys[i - 1] + xs[i];
+  }
+}
+
 void run_block_scan(int argc, char **argv) {
-  num_t *xs = (num_t *)malloc(LENGTH * sizeof(num_t));
-  num_t *ys = (num_t *)malloc(LENGTH * sizeof(num_t));
+  size_t array_size = LENGTH * sizeof(num_t);
+
+  num_t *xs = (num_t *)malloc(array_size);
+  num_t *truth_ys = (num_t *)malloc(array_size);
   for (size_t i = 0; i < LENGTH; i++) {
     xs[i] = 1;
   }
+  host_bscan(xs, truth_ys, LENGTH);
 
-  host_bscan(xs, ys, LENGTH);
+  num_t *device_xs = NULL;
+  CUDA_ERROR(cudaMalloc((void **) &device_xs, array_size), "Couldn't allocate device_xs");
+  CUDA_ERROR(
+      cudaMemcpy(device_xs, xs, array_size, cudaMemcpyHostToDevice),
+      "Couldn't copy to device_xs");
+  num_t *device_ys = NULL;
+  CUDA_ERROR(cudaMalloc((void **) &device_ys, array_size), "Couldn't allocate device_ys");
+
+  int num_blocks = ceil(float(LENGTH) / float(BLOCK_SIZE));
+  printf("%d\n", array_size);
+  single_thread_bscan<<<num_blocks, BLOCK_SIZE>>>(device_xs, device_ys, LENGTH);
+
+  cudaDeviceSynchronize();
+
+  num_t *ys = (num_t *)malloc(array_size);
+  CUDA_ERROR(
+      cudaMemcpy(ys, device_ys, array_size, cudaMemcpyDeviceToHost),
+      "Couldn't copy to ys");
+
+  for (size_t i = 0; i < LENGTH; i++) {
+    if (truth_ys[i] != ys[i]) {
+      fprintf(
+          stderr,
+          "Incorrect value at index %d: Expected %f, got %f\n",
+          i,
+          truth_ys[i],
+          ys[i]);
+    }
+  }
 
   printf("Result: ");
   for (size_t i = 0; i < LENGTH; i++) {
